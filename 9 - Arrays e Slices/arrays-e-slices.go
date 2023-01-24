4e860b7036519105906bb46757863b42f8dd11fb
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-01-24 13:33:32 -0300

    Golang fundamentos básicos pt 9.2

--- 9 - Arrays e Slices/arrays-e-slices.go
+++ 9 - Arrays e Slices/arrays-e-slices.go
@@ -31,4 +31,19 @@ func main() {
 
 	array2[1] = "Posição alterada"
 	fmt.Println(slice2)
+
+	// Arrays internos
+	fmt.Println("--------------------------------")
+	slice3	:= make([]float32, 10, 11)
+	fmt.Println(slice3)
+
+	slice3 = append(slice3, 5)
+	slice3 = append(slice3, 6)
+	fmt.Println(len(slice3)) // length
+	fmt.Println(cap(slice3)) // capacidade
+
+	slice4 := make([]float32, 5)
+	slice4 = append(slice4, 6)
+	fmt.Println(len(slice4))
+	fmt.Println(cap(slice4))
 }
