9da5ec2db7eb3220403e01292519fffce6d358f1
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-01-26 13:54:11 -0300

    Golang concorrencia 18.2

---
+++ 18 - Concorrencia/waitgroup/waitgroup.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var waitgroup sync.WaitGroup
+
+	waitgroup.Add(2) // quantidade de goroutines para serem executadas em concorrencia
+
+	go func () {
+		escrever("Olá mundo!")
+		waitgroup.Done() // -1
+	}()
+
+	go func() {
+		escrever("Programando em GO!")
+		waitgroup.Done() // -1
+	}()
+
+	waitgroup.Wait()
+
+}
+
+func escrever(texto string) {
+	for i:= 0; i < 5; i++ {
+		fmt.Println(texto)
+		time.Sleep(time.Second)
+	}
+}
