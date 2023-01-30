0b7d2bc36b28114c60870165068cf7886f6526f9
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-01-30 08:40:02 -0300

    Golang banco de dados - final

--- 23 - Banco de Dados/banco-de-dados.go
+++ 23 - Banco de Dados/banco-de-dados.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
 func main() {
-	$END$
+	stringConexao := "golang:golang@/devbook?charset=utf8&parseTime=True&loc=Local"
+
+	db, erro := sql.Open("mysql", stringConexao)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer db.Close()
+
+	if erro = db.Ping(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	fmt.Println("Conexão aberta com o banco de dados")
+
+	linhas, erro := db.Query("select * from usuarios")
+	if erro != nil {
+		log.Fatal(erro)
+	}
+	defer linhas.Close()
+
+	fmt.Println(linhas)
+
 }
